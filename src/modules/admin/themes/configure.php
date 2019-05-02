6d80dede5e222e8d2abd507a04214fd5f85e138a
GaalexxC <gary@devcu.com>
2019-05-02 00:13:31 -0400

    skel

---
+++ src/modules/admin/themes/configure.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       FrontPage Themes Configure Controller
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	FrontPage
+ * @version     1.0.0
+ * @source      https://github.com/devCU/IPS-FrontPage
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     25 APR 2019
+ * @Updated    01 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\frontpage\modules\admin\themes;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Themes Configure
+ */
+class _configure extends \IPS\Dispatcher\Controller
+{
+	/**
+	 * Execute
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		\IPS\Dispatcher::i()->checkAcpPermission( 'settings_manage' );
+		parent::execute();
+	}
+
+	protected function manage()
+	{
+		\IPS\Dispatcher::i()->checkAcpPermission( 'settings_manage' );
+
+		$view = $this->_manageConfigure();
+
+		\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack('head_frontpage_configure');
+		\IPS\Output::i()->output = $view;
+}
+	/**
+	 * Themes Configure
+	 *
+	 * @return	void
+	 */
+	protected function _manageConfigure()
+	{
+		
+		/* Display */
+		return \IPS\Theme::i()->getTemplate( 'themes', 'frontpage', 'admin' )->configure();
+	}
+	
+}
