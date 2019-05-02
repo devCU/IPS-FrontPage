53805851cada9e81fe87e7b595b7f7863e4b106d
GaalexxC <gary@devcu.com>
2019-05-02 00:12:49 -0400

    skel

--- src/Application.php
+++ src/Application.php
@@ -3,7 +3,7 @@
  *     Support this Project... Keep it free! Become an Open Source Patron
  *                       https://www.patreon.com/devcu
  *
- * @brief       IPS FrontPage Application Class
+ * @brief       FrontPage Application Class
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
@@ -13,7 +13,7 @@
  * @source      https://github.com/devCU/IPS-FrontPage
  * @Issue Trak  https://www.devcu.com/devcu-tracker/
  * @Created     25 APR 2019
- * @Updated     25 APR 2019
+ * @Updated    10 MAY 2019
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -30,7 +30,6 @@
  *    along with this program.  If not, see http://www.gnu.org/licenses/
  */
  
- 
 namespace IPS\frontpage;
 
 /**
@@ -38,5 +37,74 @@ namespace IPS\frontpage;
  */
 class _Application extends \IPS\Application
 {
+	/**
+	 * Init
+	 *
+	 * @return	void
+	 */
+	public function init()
+	{
+		/* If the viewing member cannot view the board (ex: guests must login first), then send a 404 Not Found header here, before the Login page shows in the dispatcher */
+		if ( \IPS\Dispatcher::hasInstance() AND \IPS\Dispatcher::i()->controllerLocation === 'front')
+		{
+			if ( !\IPS\Member::loggedIn()->group['g_view_board'] and ( \IPS\Request::i()->module == 'frontpage' and \IPS\Request::i()->controller == 'main' and \IPS\Request::i()->do == 'rss' )
+			or ( \IPS\Member::loggedIn()->members_bitoptions['remove_frontpage_access'] )
+			)
+			{
+				\IPS\Output::i()->error( 'node_error', '2G218/1', 404, '' );
+			}
+		}
+	}
+	/**
+	 * [Node] Get Icon for tree
+	 *
+	 * @note	Return the class for the icon (e.g. 'globe')
+	 * @return	string|null
+	 */
+	protected function get__icon()
+	{
+		return 'paper-plane';
+	}
 	
+	/**
+	 * Default front navigation
+	 *
+	 * @code
+	 	
+	 	// Each item...
+	 	array(
+			'key'		=> 'Example',		// The extension key
+			'app'		=> 'core',			// [Optional] The extension application. If ommitted, uses this application	
+			'config'	=> array(...),		// [Optional] The configuration for the menu item
+			'title'		=> 'SomeLangKey',	// [Optional] If provided, the value of this language key will be copied to menu_item_X
+			'children'	=> array(...),		// [Optional] Array of child menu items for this item. Each has the same format.
+		)
+	 	
+	 	return array(
+		 	'rootTabs' 		=> array(), // These go in the top row
+		 	'browseTabs'	=> array(),	// These go under the Browse tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'browseTabsEnd'	=> array(),	// These go under the Browse tab after all other items on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'activityTabs'	=> array(),	// These go under the Activity tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Activity tab may not exist)
+		)
+	 * @endcode
+	 * @return array
+	 */
+	public function defaultFrontNavigation()
+	{
+		$browseTabs = array();
+		
+		try
+		{
+			$defaultPage = \IPS\frontpage\Themes\Pages::getDefaultPage();
+			$browseTabs[] = array( 'key' => 'Pages', 'config' => array( 'menu_content_page' => $defaultPage->id, 'menu_title_page_type' => 0 ) );
+		}
+		catch( \OutOfRangeException $ex ) { }
+		
+		return array(
+			'rootTabs'		=> array(),
+			'browseTabs'	=> $browseTabs,
+			'browseTabsEnd'	=> array(),
+			'activityTabs'	=> array()
+		);
+	}
 }
