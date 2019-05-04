18d311223575976efd60e60c4dfc18d8dbd4af02
GaalexxC <gary@devcu.com>
2019-05-04 17:05:18 -0400

    Tasks

---
+++ src/frontpage/tasks/FrontpageUpdateRecords.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       FrontPage UpdateRecords Task
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	FrontPage
+ * @version     1.0.0
+ * @source      https://github.com/devCU/IPS-FrontPage
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     25 APR 2019
+ * @Updated     04 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\frontpage\tasks;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * FrontPage UpdateRecords Task
+ */
+class _FrontpageUpdateRecords extends \IPS\Task
+{
+	/**
+	 * Execute
+	 *
+	 * If ran successfully, should return anything worth logging. Only log something
+	 * worth mentioning (don't log "task ran successfully"). Return NULL (actual NULL, not '' or 0) to not log (which will be most cases).
+	 * If an error occurs which means the task could not finish running, throw an \IPS\Task\Exception - do not log an error as a normal log.
+	 * Tasks should execute within the time of a normal HTTP request.
+	 *
+	 * @return	mixed	Message to log or NULL
+	 * @throws	\IPS\Task\Exception
+	 */
+	public function execute()
+	{
+		foreach( \IPS\frontpage\Databases::databases() as $database )
+		{
+			$recordClass = 'IPS\frontpage\Records' . $database->id;
+			
+			$fixedFields = $database->fixed_field_perms;
+			if ( ! \in_array( 'record_expiry_date', array_keys( $fixedFields ) ) )
+			{
+				continue;
+			}
+			
+			$permissions = $fixedFields['record_expiry_date'];
+			
+			if ( ! empty( $permissions['visible'] ) )
+			{
+				foreach( \IPS\Db::i()->select( '*', $recordClass::$databaseTable, array( 'record_expiry_date > 0 and record_expiry_date <= ? and record_approved=1', time() ),'primary_id_field ASC', 250 ) as $row )
+				{
+					$record = $recordClass::constructFromData( $row );
+					$record->hide( FALSE );
+				}
+			}
+		}
+		
+		return NULL;
+	}
+	
+	/**
+	 * Cleanup
+	 *
+	 * If your task takes longer than 15 minutes to run, this method
+	 * will be called before execute(). Use it to clean up anything which
+	 * may not have been done
+	 *
+	 * @return	void
+	 */
+	public function cleanup()
+	{
+		
+	}
+}
