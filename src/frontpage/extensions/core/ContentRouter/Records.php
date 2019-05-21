ab230cd112cf99cf1b051f6fc3959fd1de99baee
GaalexxC <gary@devcu.com>
2019-05-20 20:57:54 -0400

    Release 1.0.0

---
+++ src/frontpage/extensions/core/ContentRouter/Records.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief		FrontPage Router extension: Records
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	FrontPage
+ * @version     1.0.0
+ * @source      https://github.com/devCU/IPS-FrontPage
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     25 APR 2019
+ * @Updated     20 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\frontpage\extensions\core\ContentRouter;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * @brief	Content Router extension: Records
+ */
+class _Records
+{
+	/**
+	 * @brief	Content Item Classes
+	 */
+	public $classes = array();
+	
+	/**
+	 * Constructor
+	 *
+	 * @param	\IPS\Member|IPS\Member\Group|NULL	$member		If checking access, the member/group to check for, or NULL to not check access
+	 * @return	void
+	 */
+	public function __construct( $member = NULL )
+	{
+		try
+		{
+			foreach ( \IPS\frontpage\Databases::databases() as $id => $database )
+			{
+				if( $database->fpage_id )
+				{
+					if ( !$member or $database->can( 'view', $member ) )
+					{
+						$this->classes[] = 'IPS\frontpage\Records' . $id;
+					}
+				}
+			}
+		}
+		catch ( \Exception $e ) {} // If you have not upgraded contents but it is installed, this throws an error
+	}
+}
