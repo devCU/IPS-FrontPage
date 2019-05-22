499c25d042ac2074de449fb9f004894c66838f1b
GaalexxC <gary@devcu.com>
2019-05-22 02:33:03 -0400

    RC 1.0.0

--- src/frontpage/extensions/core/FrontNavigation/Fpages.php
+++ src/frontpage/extensions/core/FrontNavigation/Fpages.php
@@ -6,11 +6,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4+
  * @subpackage	FrontPage
- * @version     1.0.0
+ * @version     1.0.0 RC
  * @source      https://github.com/devCU/IPS-FrontPage
  * @Issue Trak  https://www.devcu.com/devcu-tracker/
  * @Created     25 APR 2019
- * @Updated     20 MAY 2019
+ * @Updated     22 MAY 2019
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
