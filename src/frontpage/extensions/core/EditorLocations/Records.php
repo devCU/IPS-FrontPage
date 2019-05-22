3205c94be23c88f2335831fe4c8f70c8eb65d70e
GaalexxC <gary@devcu.com>
2019-05-22 02:26:28 -0400

    RC 1.0.0

---
+++ src/frontpage/extensions/core/EditorLocations/Records.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief		Editor Extension: Record Form
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	FrontPage
+ * @version     1.0.0 RC
+ * @source      https://github.com/devCU/IPS-FrontPage
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     25 APR 2019
+ * @Updated     22 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\frontpage\extensions\core\EditorLocations;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Editor Extension: Record Content
+ */
+class _Records
+{
+	/**
+	 * @brief	Flag to indicate we don't want to be listed as a selectable area when configuring buttons
+	 */
+	public static $buttonLocation	= FALSE;
+
+	/**
+	 * Can we use HTML in this editor?
+	 *
+	 * @param	\IPS\Member	$member	The member
+	 * @return	bool|null	NULL will cause the default value (based on the member's permissions) to be used, and is recommended in most cases. A boolean value will override that.
+	 */
+	public function canUseHtml( $member )
+	{
+		return NULL;
+	}
+	
+	/**
+	 * Can we use attachments in this editor?
+	 *
+	 * @param	\IPS\Member	$member	The member
+	 * @param	\IPS\Helpers\Form\Editor $editor The editor instance
+	 * @return	bool|null	NULL will cause the default value (based on the member's permissions) to be used, and is recommended in most cases. A boolean value will override that.
+	 */
+	public function canAttach( $member, $editor )
+	{
+		if( !$editor->options['allowAttachments'] )
+		{
+			return FALSE;
+		}
+
+		return NULL;
+	}
+
+	/**
+	 * Permission check for attachments
+	 *
+	 * @param	\IPS\Member	$member		The member
+	 * @param	int|null	$id1		Primary ID
+	 * @param	int|null	$id2		Secondary ID
+	 * @param	string|null	$id3		Arbitrary data
+	 * @param	array		$attachment	The attachment data
+	 * @param	bool		$viewOnly	If true, just check if the user can see the attachment rather than download it
+	 * @return	bool
+	 */
+	public function attachmentPermissionCheck( $member, $id1, $id2, $id3, $attachment, $viewOnly=FALSE )
+	{
+		if ( ! $id3 )
+		{
+			throw new \OutOfRangeException;
+		}
+		
+		try
+		{
+			$recordClass = '\IPS\frontpage\Records' . $id3;
+			
+			return $recordClass::load( $id1 )->canView( $member );
+		}
+		catch ( \OutOfRangeException $e )
+		{
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * Attachment lookup
+	 *
+	 * @param	int|null	$id1	Primary ID
+	 * @param	int|null	$id2	Secondary ID
+	 * @param	string|null	$id3	Arbitrary data
+	 * @return	\IPS\Http\Url|\IPS\Content|\IPS\Node\Model
+	 * @throws	\LogicException
+	 */
+	public function attachmentLookup( $id1, $id2, $id3 )
+	{
+		try
+		{
+			if ( $id3 )
+			{
+				$recordClass = '\IPS\frontpage\Records' . $id3;
+				$return = $recordClass::load( $id1 );
+				$return->url(); // Need to check that won't throw an exception later, which might happen if the database no longer has a fpage
+				return $return;
+			}
+			else
+			{
+				return FALSE;
+			}
+		}
+		catch ( \Exception $e )
+		{
+			return FALSE;
+		}
+	}
+}
