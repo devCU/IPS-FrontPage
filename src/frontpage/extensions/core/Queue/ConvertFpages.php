1c1ae855624f38e2113fa84c2fa0c4900b44c4ab
GaalexxC <gary@devcu.com>
2019-05-22 02:27:18 -0400

    RC 1.0.0

---
+++ src/frontpage/extensions/core/Queue/ConvertFpages.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief		Background Task: Convert bbcode fpages to html fpages post-3.4 upgrade
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	FrontPage
+ * @version     1.0.0 RC
+ * @source      https://github.com/devCU/IPS-FrontPage
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     25 APR 2019
+ * @Updated     22 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\frontpage\extensions\core\Queue;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Background Task: Convert bbcode fpages to html fpages post-3.4 upgrade
+ */
+class _ConvertFpages
+{
+	/**
+	 * Run Background Task
+	 *
+	 * @param	mixed						$data	Data as it was passed to \IPS\Task::queue()
+	 * @param	int							$offset	Offset
+	 * @return	int							New offset
+	 * @throws	\IPS\Task\Queue\OutOfRangeException	Indicates offset doesn't exist and thus task is complete
+	 */
+	public function run( $data, $offset )
+	{
+		foreach( \IPS\Db::i()->select( '*', 'frontpage_fpages', array( 'fpage_type=?', 'bbcode' ) ) as $fpage )
+		{
+			$update = array(
+				'fpage_content' => \IPS\Text\LegacyParser::parseStatic( $fpage['fpage_content'] ),
+				'fpage_type'    => 'html'
+			);
+			
+			\IPS\Db::i()->update( 'frontpage_fpages', $update, array( 'fpage_id=?', $fpage['fpage_id'] ) );
+		}
+
+		throw new \IPS\Task\Queue\OutOfRangeException;
+	}
+	
+	/**
+	 * Get Progress
+	 *
+	 * @param	mixed					$data	Data as it was passed to \IPS\Task::queue()
+	 * @param	int						$offset	Offset
+	 * @return	array( 'text' => 'Doing something...', 'complete' => 50 )	Text explaining task and percentage complete
+	 * @throws	\OutOfRangeException	Indicates offset doesn't exist and thus task is complete
+	 */
+	public function getProgress( $data, $offset )
+	{
+		return array( 'text' => \IPS\Member::loggedIn()->language()->addToStack( 'converting_fpages_34x' ), 'complete' => 100 );
+	}	
+}
