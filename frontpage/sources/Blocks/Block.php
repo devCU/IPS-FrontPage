e30ddeb77e56b507fa17e53a974416829628d82c
GaalexxC <gary@devcu.com>
2020-03-20 12:43:22 -0400

    1.0.4 RC1 Stable

--- frontpage/sources/Blocks/Block.php
+++ frontpage/sources/Blocks/Block.php
@@ -9,11 +9,11 @@
  * @license     GNU General Public License v3.0
  * @package     Invision Community Suite 4.4+
  * @subpackage	FrontPage
- * @version     1.0.0 RC
+ * @version     1.0.4 Stable
  * @source      https://github.com/devCU/IPS-FrontPage
  * @Issue Trak  https://www.devcu.com/devcu-tracker/
  * @Created     25 APR 2019
- * @Updated     22 MAY 2019
+ * @Updated     20 MAR 2020
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -260,7 +260,7 @@ class _Block extends \IPS\Node\Model implements \IPS\Node\Permissions
 					$block->widget()->template( array( $block, 'getTemplate' ) );
 				}
 
-				return $block->widget()->render();
+				return \IPS\Widget::parseOutput( $block->widget()->render() );
 			}
 		}
 		catch( \OutOfRangeException $ex )
