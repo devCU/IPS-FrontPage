d90353f34042c6635ece4641858b7797b3014ea9
GaalexxC <gary@devcu.com>
2020-10-19 10:48:56 -0400

    IPS 4.5x

--- frontpage/sources/Theme/Theme.php
+++ frontpage/sources/Theme/Theme.php
@@ -1,19 +1,19 @@
 <?php
 /**
  *     Support this Project... Keep it free! Become an Open Source Patron
- *                      https://www.devcu.com/donate/
+ *                       https://www.devcu.com/donate
  *
  * @brief		IN_DEV Skin Set
  * @author      Gary Cornell for devCU Software Open Source Projects
  * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
  * @license     GNU General Public License v3.0
- * @package     Invision Community Suite 4.4.10 FINAL
+ * @package     Invision Community Suite 4.5x
  * @subpackage	FrontPage
  * @version     1.0.5 Stable
  * @source      https://github.com/devCU/IPS-FrontPage
  * @Issue Trak  https://www.devcu.com/devcu-tracker/
  * @Created     25 APR 2019
- * @Updated     12 AUG 2020
+ * @Updated     19 OCT 2020
  *
  *                    GNU General Public License v3.0
  *    This program is free software: you can redistribute it and/or modify       
@@ -301,9 +301,9 @@ class _Theme extends \IPS\Theme
 	public function getRawTemplates( $app=array(), $location=array(), $group=array(), $returnType=null, $returnThisSetOnly=false )
 	{
 		$returnType = ( $returnType === null )  ? self::RETURN_ALL   : $returnType;
-		$app        = ( \is_string( $app )      AND $app != ''      ) ? array( $app )      : $app;
-		$location   = ( \is_string( $location ) AND $location != '' ) ? array( $location ) : $location;
-		$group      = ( \is_string( $group )    AND $group != ''    ) ? array( $group )    : $group;
+		$app        = ( \is_string( $app )      AND ! empty( $app ) ) ? array( $app )      : $app;
+		$location   = ( \is_string( $location ) AND ! empty( $location ) ) ? array( $location ) : $location;
+		$group      = ( \is_string( $group )    AND ! empty( $group ) ) ? array( $group )    : $group;
 		$where      = array();
 		$templates  = array();
 
